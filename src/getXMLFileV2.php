3050ea3d17c122d5891768438c40fbb332b8ad03
ngocanh1233 <ngocanh1233@gmail.com>
2024-01-03 22:32:12 +0700

    chore: Update

--- src/getXMLFileV2.php
+++ src/getXMLFileV2.php
@@ -15,31 +15,34 @@ $dt_ACC_Street = $_POST['dt_ACC_Street'];
 $dt_ACC_Ort = $_POST['dt_ACC_Ort'];
 $dz_ACC_PLZ = $_POST['dz_ACC_PLZ'];
 $dt_ACC_UstID = $_POST['dt_ACC_UstID'];
-$dt_Land = $_POST['dt_Land'];
 $dt_ACC_Tele = $_POST['dt_ACC_Tele'];
 $dt_ACC_Email = $_POST['dt_ACC_Email'];
 $dz_Seller_Order_reference = $_POST['dz_Seller_Order_reference'];
-$dd_Delivery_date = $_POST['dd_Delivery_date'];
-
 $dz_Description_payment_first = $_POST['dz_Description_payment_first'];
+
 
 $dt_Name_recipien = $_POST['dt_Name_recipien'];
 $dt_recipien_address = $_POST['dt_recipien_address'];
+$dz_Purchaser_Order_reference = $_POST['dz_Purchaser_Order_reference'];
+$dd_Delivery_date = $_POST['dd_Delivery_date'];
+$dt_additional_address =  $_POST['dt_additional_address'];
 $dt_recipien_PLZ = $_POST['dt_recipien_PLZ'];
 $dt_recipien_city = $_POST['dt_recipien_city'];
-$dz_Description_payment = $_POST['dz_Description_payment'];
 $dt_recipien_tele =  $_POST['dt_recipien_tele'];
 $dt_recipien_Email =  $_POST['dt_recipien_Email'];
-$dt_additional_address =  $_POST['dt_additional_address'];
+$dz_Description_payment = $_POST['dz_Description_payment'];
 
 $dFzu_TotalPrice = $_POST['dFzu_TotalPrice'];
 $dFzu_SumBeforeTax = $_POST['dFzu_SumBeforeTax'];
-$dFzu_SumRabatt = $_POST['dFzu_SumRabatt'];
-$dz_Amount_already_paid = $_POST['dz_Amount_already_paid'];
+$dFzu_SumRabatt = !$_POST['dFzu_SumRabatt'] ? 0.0 : $_POST['dFzu_SumRabatt'];
+$dz_Rabatt = $_POST['dz_Rabatt'] == '' ? 0.0 : $_POST['dz_Rabatt'];
+$dFzu_Price_Discount_percent = $_POST['dFzu_Price_Discount_percent'] == '' ? 0.0 : $_POST['dFzu_Price_Discount_percent'];
+$dz_Amount_already_paid = $_POST['dz_Amount_already_paid'] == '' ? 0.0 : $_POST['dz_Amount_already_paid'];
 $dFzu_TotalTax = $_POST['dFzu_TotalTax'];
+$dt_Land = $_POST['dt_Land'];
 $dt_CodeInvoice = $_POST['dt_CodeInvoice'];
+$dd_due_date_Invoice_amount = $_POST['dd_due_date_Invoice_amount'];
 $dz_Tax = $_POST['dz_Tax'];
-$dz_Purchaser_Order_reference = $_POST['dz_Purchaser_Order_reference'];
 
 $dataJSON = str_replace("'", "",$_POST['dt_Json']);
 
@@ -51,26 +54,36 @@ $newDate = date("Ymd", strtotime($originalDate));
 $deliveryDate = $dd_Delivery_date;
 $newDateDelivery = date("Ymd", strtotime($deliveryDate));
 
+$dueDate = $dd_due_date_Invoice_amount;
+$newDueDate = date("Ymd", strtotime($dueDate));
 
 $document = ZugferdDocumentBuilder::CreateNew(ZugferdProfiles::PROFILE_XRECHNUNG_3);
-$document
-    ->setDocumentInformation($dt_RECHFormat_Nr, "380", \DateTime::createFromFormat("Ymd", $newDate), "EUR")
-    ->addDocumentNote($dz_Description_payment_first)
-    //->addDocumentNote($dt_ACC_Name . PHP_EOL . $dt_ACC_Street . PHP_EOL . $dz_ACC_PLZ." ". $dt_ACC_Ort . PHP_EOL . $dt_Land)
-    ->setDocumentSupplyChainEvent(\DateTime::createFromFormat('Ymd', $newDateDelivery))
-    ->addDocumentPaymentMean(ZugferdPaymentMeans::UNTDID_4461_58, null, null, null, null, null, $dt_CodeInvoice, null, null, null)
-    ->setDocumentSeller($dt_ACC_Name, null)
-    ->addDocumentSellerTaxRegistration("VA", $dt_ACC_UstID)
-    ->setDocumentSellerAddress($dt_ACC_Street, "", "", $dz_ACC_PLZ , $dt_ACC_Ort, $dt_Land)
-    ->setDocumentSellerContact($dt_ACC_Name, "", $dt_ACC_Tele, null,$dt_ACC_Email)
-    ->setDocumentSellerOrderReferencedDocument($dz_Seller_Order_reference)
-    ->setDocumentBuyer($dt_Name_recipien, null)
-    ->setDocumentBuyerReference($dz_Purchaser_Order_reference)
-    ->setDocumentBuyerAddress($dt_recipien_address, $dt_additional_address, "", $dt_recipien_PLZ, $dt_recipien_city, $dt_Land)
-    ->setDocumentBuyerContact($dt_Name_recipien, "", "", $dt_recipien_tele, "", $dt_recipien_Email)
-	->addDocumentTax("S", "VAT", $dFzu_SumBeforeTax, $dFzu_TotalTax, $dz_Tax)
-    ->setDocumentSummation($dFzu_TotalPrice, $dFzu_TotalPrice , $dFzu_SumBeforeTax, 0.0, 0.0, $dFzu_SumBeforeTax, $dFzu_TotalTax, null, 0.0)
-    ->addDocumentPaymentTerm($dz_Description_payment);
+$document->setDocumentInformation($dt_RECHFormat_Nr, "380", \DateTime::createFromFormat("Ymd", $newDate), "EUR");
+$document->addDocumentNote($dz_Description_payment_first);
+$document->addDocumentNote($dt_ACC_Name . PHP_EOL . $dt_ACC_Street . PHP_EOL . $dz_ACC_PLZ." ". $dt_ACC_Ort . PHP_EOL . $dt_Land);
+if($dd_Delivery_date != '') {
+    $document->setDocumentSupplyChainEvent(\DateTime::createFromFormat('Ymd', $newDateDelivery));
+}
+$document->addDocumentPaymentMean(ZugferdPaymentMeans::UNTDID_4461_58, null, null, null, null, null, $dt_CodeInvoice, null, null, null);
+$document->setDocumentSeller($dt_ACC_Name, null);
+$document->addDocumentSellerTaxRegistration("VA", $dt_ACC_UstID);
+$document->setDocumentSellerAddress($dt_ACC_Street, "", "", $dz_ACC_PLZ , $dt_ACC_Ort, $dt_Land);
+$document->setDocumentSellerContact($dt_ACC_Name, "", $dt_ACC_Tele, null,$dt_ACC_Email);
+$document->setDocumentSellerOrderReferencedDocument($dz_Seller_Order_reference);
+$document->setDocumentBuyer($dt_Name_recipien, null);
+$document->setDocumentBuyerReference($dz_Purchaser_Order_reference);
+$document->setDocumentBuyerAddress($dt_recipien_address, $dt_additional_address, "", $dt_recipien_PLZ, $dt_recipien_city, $dt_Land);
+$document->setDocumentBuyerContact($dt_Name_recipien, "", "", $dt_recipien_tele, "", $dt_recipien_Email);
+$document->addDocumentTax("S", "VAT", $dFzu_Price_Discount_percent, $dFzu_TotalTax, $dz_Tax);
+$document->addDocumentAllowanceCharge($dFzu_SumRabatt, false, "S", "VAT", $dz_Tax, null, $dz_Rabatt, $dFzu_SumBeforeTax);
+$document->setDocumentSummation($dFzu_TotalPrice, $dFzu_TotalPrice , $dFzu_SumBeforeTax, 0.0, $dFzu_SumRabatt, $dFzu_Price_Discount_percent, $dFzu_TotalTax, null,$dz_Amount_already_paid);
+
+if($dueDate == '') {
+    $document->addDocumentPaymentTerm($dz_Description_payment);
+} else {
+    $document->addDocumentPaymentTerm($dz_Description_payment,\DateTime::createFromFormat("Ymd", $newDueDate));
+}
+
 
 	foreach($dt_Json as $key => $value){
 		$document
@@ -82,7 +95,6 @@ $document
 		->addDocumentPositionTax('S', 'VAT', $dz_Tax)
 		->setDocumentPositionLineSummation($value['Netto']);
 	}
-
 
 header('Content-Type: application/xml');
 header('Content-Length: '.strlen( $document ));
